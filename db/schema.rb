d86253858f08b85852acdee974b832d4d10fda14
Uduak Essien <acushla4real@gmail.com>
2021-06-29 22:15:04 +0100

    Fix rubocop expo

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_202817) do
-
+ActiveRecord::Schema.define(version: 20_210_629_202_817) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "submissions", force: :cascade do |t|
-    t.string "word"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'submissions', force: :cascade do |t|
+    t.string 'word'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
