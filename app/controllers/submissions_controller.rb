66c2a80a817504cd7500e1e70eb6e6452ffeef50
Uduak Essien <acushla4real@gmail.com>
2021-06-29 22:12:33 +0100

    Add create and challenge method to submissions controller

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,5 +1,36 @@
 class SubmissionsController < ApplicationController
-    def index
-      @submission = Submission.new
+  def index
+    @submission = Submission.new
+  end
+
+  def create
+    @submission = Submission.new(submission_params)
+    if @submission.save
+      redirect_to "/#{@submission.word}"
+    else
+      redirect_to root_path, flash: { alert: @submission.errors.first.full_message }
     end
+  end
+
+  def challenge
+    @submission = Submission.new
+    @new_word = params[:word]
+
+    @results = []
+    current = []
+    @new_word.split('').each do |word|
+      if current.include?(word)
+        @results = current if current.length > @results.length
+        current = []
+      end
+      current << word
+    end
+    @results
+  end
+
+  private
+
+  def submission_params
+    params.require(:submission).permit(:word)
+  end
 end
