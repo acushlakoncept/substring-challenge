0b4ca4c2b044ed6ace2149e34cd3bf7d0ed3f41e
Uduak Essien <acushla4real@gmail.com>
2021-06-29 21:56:55 +0100

    Add routes for word url

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :submissions, only: [:index]
+  root 'submissions#index'
+
+  get '/:word', to: 'submissions#challenge', as: 'challenge'
 end
