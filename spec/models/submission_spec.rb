62dc3b91ef03966bf986798bed4be0fa3de4cb80
Uduak Essien <acushla4real@gmail.com>
2021-06-30 12:27:19 +0100

    Add word with space test

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Submission, type: :model do
   it { should validate_presence_of(:word) }
+
+  it 'should fail to submit new word' do
+    new_word = Submission.new(word: "New Word").save
+    expect(new_word) == false
+  end
 end
